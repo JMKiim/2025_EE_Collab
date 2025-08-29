31e96f517ea2516157a055348cfcf8795eedb6be
Xyan <jmkacc99@gmail.com>
2025-08-29 11:28:06 +0900

    ADD: Video crop 6 & 7

--- single_video_crop.py
+++ single_video_crop.py
@@ -5,17 +5,18 @@ import subprocess
 def get_zoom5_coords(W, H, w, h):
     top_left_w    = W/2 - (3*w)/2
     top_left_h    = H/2 - h
-    bottom_left_w = W/2 - w
+    # bottom_left_w = W/2 - w
 
     return [
         (int(top_left_w),       int(top_left_h), int(w), int(h)),      # P1
         (int(top_left_w) + w,   int(top_left_h), int(w), int(h)),      # P2
         (int(top_left_w) + 2*w, int(top_left_h), int(w), int(h)),      # P3
-        (int(bottom_left_w),           int(H/2), int(w), int(h)),      # P4
-        (int(W/2),                     int(H/2), int(w), int(h)),      # P5
+        (int(top_left_w),              int(H/2), int(w), int(h)),      # P4
+        (int(top_left_w) + w,          int(H/2), int(w), int(h)),      # P5
+        (int(top_left_w) + 2*w,        int(H/2), int(w), int(h)),      # P6
     ]
 
-input_path = r"D:/2025신윤희영상정렬/24-1/B6/B6_W5_21453.mp4"
+input_path = r"D:/2025신윤희영상정렬/24-2/F/F_W2_1234.mp4"
 output_dir = "cropped_persons"
 os.makedirs(output_dir, exist_ok=True)
 
@@ -32,8 +33,8 @@ w = int(W * 0.325)
 h = int(H * 0.325)
 
 # 3) 자를 시간 범위
-start_time = "00:24:00"
-end_time   = "00:25:00"
+start_time = "00:52:18"
+end_time   = "00:52:33"
 
 # 4) 크롭 좌표 생성
 coords = get_zoom5_coords(W, H, w, h)
